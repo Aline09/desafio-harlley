dd93637b752bd14d1640b508592231bb6d5f7ff8
Aline09 <oliveira.alineod@gmail.com>
2018-11-15 13:55:19 -0200

    Primeiro commit do desafio

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+<header class="header">
+ <h1 class="header__titulo">
+ <span class="header__titulo--hashtag">#desafio100videos</span><span class="header__titulo--contador">Faltam 13 vídeos</span>
+ </h1></header>
+ <main>
+   <section class="video">
+     <div class="row">
+     <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+     </div>
+
+     <div class="row">
+     <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+     </div>
+     <div class="row">
+     <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card">
+       <img src="img/square.png" alt="" srcset="" class="video__card--square">
+    </div>
+    <div class="video__card"> 
+       <img src="img/tumbnail.jpg" alt="" srcset=""class="video__card--tumbnail">
+    </div>
+    <div class="video__card"> 
+       <img src="img/tumbnail.jpg" alt="" srcset=""class="video__card--tumbnail">
+    </div>
+     </div>
+     <div class="row">
+     <div class="video__card"> 
+       <img src="img/tumbnail.jpg" alt="" srcset=""class="video__card--tumbnail">
+    </div>
+    <div class="video__card"> 
+       <img src="img/tumbnail.jpg" alt="" srcset=""class="video__card--tumbnail">
+    </div>
+    <div class="video__card"> 
+       <img src="img/tumbnail.jpg" alt="" srcset=""class="video__card--tumbnail">
+    </div>
+    <div class="video__card"> 
+       <img src="img/tumbnail.jpg" alt="" srcset=""class="video__card--tumbnail">
+    </div>
+    <div class="video__card"> 
+       <img src="img/tumbnail.jpg" alt="" srcset=""class="video__card--tumbnail">
+    </div>
+     </div>
+     
+
+     
+   </section>
+ </main>
+</body>
+</html>
